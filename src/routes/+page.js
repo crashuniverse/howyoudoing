436a5250471814300bcf11e984e7ff23c1720a95
Priya Ranjan Singh <mail@priyaranjan.net>
2024-01-03 13:29:58 +0530

    Move data fetching to a load function

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,12 +1,17 @@
 import supabase from '$lib/supabaseClient';
 
+/** @type {import('./$types').PageLoad} */
 export async function load() {
-  const { data: statuses, error } = await supabase
+  const { data: statuses, error: statusesError } = await supabase
       .from('statuses')
       .select('*')
       .order('when');
 
+  const { data: authData, error: authError } = await supabase.auth.getUser();  
+  const user = authData?.user;
+
   return {
     statuses,
+    user,
   };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,24 @@
 <script>
   import { onMount } from 'svelte';
   import { goto } from '$app/navigation';
-  import supabase from '$lib/supabaseClient';
 
-  let isLoggedIn;
   let isLoading = true;
-  let user;
   let groupedStatuses = {};
-
+  
+  /** @type {import('./$types').PageData} */
   export let data;
+  const { statuses, user } = data;
 
   onMount(async() => {
-    const { data, error } = await supabase.auth.getUser();
-    if (!error) {
-      user = data?.user;
-      isLoggedIn = true;
-      isLoading = false;
-    } else if (error) {
-      console.error(error?.message);
-      isLoggedIn = false;
+    if (!user) {
       setTimeout(() => {
         goto('/login');
       }, 2000);
-      isLoading = false;
     }
+    isLoading = false;
   });
 
-  data?.statuses?.forEach((i) => {
+  statuses?.forEach((i) => {
     const monthAndYear = new Date(i?.when).toLocaleString('en-US', {month: 'long', year: 'numeric'});
     groupedStatuses[monthAndYear] = (groupedStatuses[monthAndYear] || []).concat(i);
   });
@@ -45,7 +37,7 @@
   </header>
   <main>
     <h2>Statuses</h2>
-    {#if data?.statuses}
+    {#if statuses?.length}
       <div>
         {#each Object.entries(groupedStatuses) as groupedItem}
           <h3>{groupedItem?.[0]}</h3>
@@ -57,10 +49,9 @@
         {/each}
       </div>
     {/if}
-    {#if isLoading && isLoggedIn === undefined}
+    {#if isLoading}
       <div>Loading...</div>
-    {/if}
-    {#if isLoading === false && !isLoggedIn}
+    {:else if isLoading === false && !user}
       <div>User not logged in. Redirecting to <a href="/login">Login</a>.</div>
     {/if}
   </main>
